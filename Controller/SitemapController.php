e1639dd151f4fbe94ebf2438bd3c01ab44ea7065
Cyberomulus <me@cyberomulus.me>
2019-01-09 22:43:15 +0100

    Adds Controller

---
+++ Controller/SitemapController.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the CyberomulusSiteMapGeneratorBundle package.
+ *
+ * (c) Brack Romain <http://www.cyberomulus.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cyberomulus\SiteMapGeneratorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Regroup all route of bundle
+ * 
+ * @author	Brack Romain <http://www.cyberomulus.me>
+ */
+class SitemapController extends AbstractController
+	{
+	/**
+	 * Route for display the sitemap index
+	 */
+	public function index()
+		{
+		return new Response("ok");
+		}
+	}
